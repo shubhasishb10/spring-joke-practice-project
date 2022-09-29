a51c028db7e0d2403e78df820732764f0207c54a
Shubhasish <shubhasishb10@gmail.com>
2022-09-29 21:03:37 +0530

    Initial commit

---
+++ src/main/java/org/shubhasish/spring/practice/spring/random/joke/SpringRandomJokeApplication.java
@@ -0,0 +1,13 @@
+package org.shubhasish.spring.practice.spring.random.joke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRandomJokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRandomJokeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/shubhasish/spring/practice/spring/random/joke/SpringRandomJokeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shubhasish.spring.practice.spring.random.joke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRandomJokeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
